bf44bf98bf739b0fa64222073be42abab9b89d1c
Marshal <olmarsh.192@gmail.com>
2024-07-04 05:49:31 +0100

    initial app setup and controllers introduction

---
+++ src/songs/songs.controller.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+@Controller('songs')
+export class SongsController {
+    @Post()
+    create() {
+        return 'create a new song';
+    }
+    @Get()
+    findAll() {
+        return 'find all songs';
+    }
+    @Get(':id')
+    findOne() {
+        return 'fetch song based on id';
+    }
+    @Put(':id')
+    update() {
+        return 'update song based on id';
+    }
+    @Delete(':id')
+    delete() {
+        return 'delete song based on id';
+    }
+}
---
+++ src/songs/songs.module.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { SongsController } from './songs.controller';
+
+@Module({
+  controllers: [SongsController]
+})
+export class SongsModule {}
