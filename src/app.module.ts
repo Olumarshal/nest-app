b4b1f6b47f56ac3ca217ca3dd3c12dd0db79d204
Marshal <olmarsh.192@gmail.com>
2024-07-04 08:05:19 +0100

    intro to middleware and error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+  }
+  
+}
--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,29 +1,48 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
-import { CreateSongDTO } from './dto1/create-song-dto';
+import { CreateSongDTO } from './dto/create-song-dto';
 
 @Controller('songs')
 export class SongsController {
-    constructor(private songsService: SongsService) {}
-    @Post()
-    create(@Body() createSongDTO: CreateSongDTO) {
-        return this.songsService.create(createSongDTO);
+  constructor(private songsService: SongsService) {}
+  @Post()
+  create(@Body() createSongDTO: CreateSongDTO) {
+    return this.songsService.create(createSongDTO);
+  }
+  @Get()
+  findAll() {
+    try {
+      return this.songsService.findAll();
+    } catch (e) {
+      throw new HttpException(
+        'server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: e,
+        },
+      );
     }
-    @Get()
-    findAll() {
-        return this.songsService.findAll();
-    }
-    @Get(':id')
-    findOne() {
-        return 'fetch song based on id';
-    }
-    @Put(':id')
-    update() {
-        return 'update song based on id';
-    }
-    @Delete(':id')
-    delete() {
-        return 'delete song based on id';
-    }
+  }
+  @Get(':id')
+  findOne() {
+    return 'fetch song based on id';
+  }
+  @Put(':id')
+  update() {
+    return 'update song based on id';
+  }
+  @Delete(':id')
+  delete() {
+    return 'delete song based on id';
+  }
 }
